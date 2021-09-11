1902e5b33d7a62eac68f47e3c34f0a1509c38a73
Jonathan Share <jonny@sharebear.co.uk>
2021-09-11 20:14:07 +0000

    feat: Parse files found via glob
    Just printing output for now, will hook up to app-insights tomorrow.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
+import * as fs from 'fs'
+import parseSurefireXML from './parser'
 
 async function run(): Promise<void> {
   try {
@@ -12,7 +14,9 @@ async function run(): Promise<void> {
       globOptions
     )
     for await (const file of globber.globGenerator()) {
-      core.info(file)
+      const data = await fs.promises.readFile(file)
+      const result = await parseSurefireXML(data)
+      core.info(JSON.stringify(result))
     }
   } catch (error) {
     if (error instanceof Error) {
