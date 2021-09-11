2b95de95b93c8d31372f6573c58fd9c9c6e0e2e9
Jonathan Share <jonny@sharebear.co.uk>
2021-09-11 20:08:42 +0000

    feat: Configure input parameter and use it to look for files

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as glob from '@actions/glob'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const globOptions = {
+      followSymbolicLinks:
+        core.getInput('follow-symbolic-links').toUpperCase() !== 'FALSE'
+    }
+    const globber = await glob.create(
+      core.getInput('report_paths'),
+      globOptions
+    )
+    for await (const file of globber.globGenerator()) {
+      core.info(file)
+    }
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
